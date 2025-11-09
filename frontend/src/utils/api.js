f1156dfbc96c9f8504bd9c130cdb5bfc2f2ef2c9
Freezer9 <farismuhammadabd@gmail.com>
2025-11-09 20:08:53 +0700

    fix: check args #3

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,10 +1,6 @@
-const API_BASE_URL =
-  process.env.REACT_APP_API_URL || "https://stark-backend.legain.id";
-
-console.log("API_BASE_URL:", API_BASE_URL); // Debug log
+const API_BASE_URL = process.env.REACT_APP_API_URL;
 
 export const fetchAPI = async (endpoint) => {
   const fullUrl = `${API_BASE_URL}${endpoint}`;
-  console.log("Fetching:", fullUrl); // Debug log
   return fetch(fullUrl);
 };
